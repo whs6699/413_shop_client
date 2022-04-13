307f3c4620c20dd1845861b5f43112347886ea8a
Whs <3391552994@qq.com>
2022-04-13 15:52:25 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+    // lintOnSave: false, // 关闭ESLint的规则检查
+    lintOnSave: 'warning', // 输出提示错误, 但项目继续运行
 })
